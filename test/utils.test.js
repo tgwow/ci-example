e93c1e01c0a2269511a6db216f8f45c02906399d
Thiago Oliveira <thiago.2809@outlook.com.br>
2021-03-18 22:47:31 -0300

    feat: fix bug on function 2 and add tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -10,10 +10,10 @@ describe('Utils', () => {
 			    expect(gerarNumeroAleatorio(-20, -5))
 			    .toBe(-1);
 		    });
-	    test('inicio não pode ser maior que o fim', () => {
-		    expect(gerarNumeroAleatorio(20, 5))
-		    .toBe(-1);
-	    });
+		    test('inicio não pode ser maior que o fim', () => {
+			    expect(gerarNumeroAleatorio(20, 5))
+			    .toBe(-1);
+		    });
     });
 
     describe('acharCaracter', () => {
@@ -21,5 +21,25 @@ describe('Utils', () => {
             expect(acharCaracter(-4, 'abcde', 'c'))
                 .toBe('comprimento invalido');
         });
+
+		    test('comprimento invalido', () => {
+			    expect(acharCaracter(26, 'abcde', 'c'))
+			    .toBe('comprimento invalido');
+		    });
+
+		    test('comprimento fornecido diferente do comprimento real', () => {
+			    expect(acharCaracter(6, 'abcde', 'c'))
+			    .toBe('comprimento fornecido diferente do comprimento real');
+		    });
+
+		    test('caracter nao está presente na cadeia', () => {
+			    expect(acharCaracter(5, 'abcde', 'f'))
+			    .toBe('caracter nao está presente na cadeia');
+		    });
+
+			    test('tamanho valido e caracter presente na cadeia', () => {
+				    expect(acharCaracter(5, 'abcde', 'c'))
+				    .toBe(2);
+			    });
     });
 });
